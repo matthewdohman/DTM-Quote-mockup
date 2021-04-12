2a4eda2917fcaea926f4bbcace681644e50df2c2
Jason Herald <jherald@pmcg.co>
2021-04-12 13:45:05 -0400

    Initial Framework Push

---
+++ dtm/apps/quote/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dtm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'quote';
+}
