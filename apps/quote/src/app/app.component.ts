910df4373d68046b5f30d50a03d56efcaeaa7d94
Jason Herald <jherald@pmcg.co>
2021-04-15 20:14:36 -0400

    Initial Build

--- apps/quote/src/app/app.component.ts
+++ apps/quote/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dtm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quote';
+
+  ngOnInit() {
+    let scriptUrl = '/assets/js/scripts-3.js';
+    let node = document.createElement('script');
+    node.src = scriptUrl;
+    node.type = 'text/javascript';
+    node.async = false;
+    node.charset = 'utf-8';
+
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
 }
